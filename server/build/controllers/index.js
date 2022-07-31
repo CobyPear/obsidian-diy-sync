fdacba5b51c3e123c6ea55a5eff86acef7253aa4
CobyPear <pear.coby@gmail.com>
2022-07-31 14:57:22 -0500

    Remove build folder from scm

--- server/build/controllers/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.vaultControllers = void 0;
-const vault_1 = require("./vault");
-Object.defineProperty(exports, "vaultControllers", { enumerable: true, get: function () { return vault_1.vaultControllers; } });
--- server/build/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = exports.router = void 0;
-const express_1 = require("express");
-const vault_1 = require("./vault");
-exports.router = (0, express_1.Router)();
-exports.router.use(vault_1.vaultRoutes);
-exports.routes = exports.router;
