5d610f0b42590e9953c2b401dce7f275fcd2d4f4
CobyPear <pear.coby@gmail.com>
2022-07-31 13:31:44 -0500

    Start developing the obsidian plugin

---
+++ plugin/main.ts
@@ -0,0 +1,155 @@
+import {
+  App,
+  Editor,
+  MarkdownView,
+  Modal,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+} from "obsidian";
+
+// Remember to rename these classes and interfaces!
+
+interface NodeSyncPluginSettings {
+  apiHost: string;
+  endpoint: string;
+  authToken?: string;
+}
+
+const DEFAULT_SETTINGS: NodeSyncPluginSettings = {
+  apiHost: "http://localhost:3001",
+  endpoint: "/api/vault",
+};
+
+export default class NodeSyncPlugin extends Plugin {
+  settings: NodeSyncPluginSettings;
+
+  async onload() {
+    await this.loadSettings();
+
+    // This adds a simple command that can be triggered anywhere
+    this.addCommand({
+      id: "open-sample-modal-simple",
+      name: "Open sample modal (simple)",
+      callback: () => {
+        new SampleModal(this.app).open();
+      },
+    });
+    // This adds an editor command that can perform some operation on the current editor instance
+    this.addCommand({
+      id: "sample-editor-command",
+      name: "Sample editor command",
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        console.log(editor.getSelection());
+        editor.replaceSelection("Sample Editor Command");
+      },
+    });
+    // This adds a complex command that can check whether the current state of the app allows execution of the command
+    this.addCommand({
+      id: "open-sample-modal-complex",
+      name: "Open sample modal (complex)",
+      checkCallback: (checking: boolean) => {
+        // Conditions to check
+        const markdownView =
+          this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (markdownView) {
+          // If checking is true, we're simply "checking" if the command can be run.
+          // If checking is false, then we want to actually perform the operation.
+          if (!checking) {
+            new SampleModal(this.app).open();
+          }
+
+          // This command will only show up in Command Palette when the check function returns true
+          return true;
+        }
+      },
+    });
+
+    // add command that syncs the vault to the api host in settings
+    this.addCommand({});
+
+    // This adds a settings tab so the user can configure various aspects of the plugin
+    this.addSettingTab(new SampleSettingTab(this.app, this));
+
+    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+    // Using this function will automatically remove the event listener when this plugin is disabled.
+    this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+      console.log("click", evt);
+    });
+
+    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+    this.registerInterval(
+      window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+    );
+  }
+
+  onunload() {}
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+}
+
+class SampleModal extends Modal {
+  constructor(app: App) {
+    super(app);
+  }
+
+  onOpen() {
+    const { contentEl } = this;
+    contentEl.setText("Woah!");
+  }
+
+  onClose() {
+    const { contentEl } = this;
+    contentEl.empty();
+  }
+}
+
+class SampleSettingTab extends PluginSettingTab {
+  plugin: NodeSyncPlugin;
+
+  constructor(app: App, plugin: NodeSyncPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+    const { containerEl } = this;
+
+    containerEl.empty();
+
+    containerEl.createEl("h2", { text: "Settings for my Nodejs Sync plugin." });
+
+    new Setting(containerEl)
+      .setName("API Host")
+      .setDesc("The location of your server")
+      .addText((text) =>
+        text
+          .setPlaceholder(
+            "Enter your api host. For example https://my.hostingprovider.myapp.com"
+          )
+          .setValue(this.plugin.settings.apiHost)
+          .onChange(async (value) => {
+            this.plugin.settings.apiHost = value;
+            await this.plugin.saveSettings();
+          })
+      );
+    new Setting(containerEl)
+      .setName("endpoint")
+      .setDesc("The location to send your vault")
+      .addText((text) =>
+        text
+          .setPlaceholder("Enter your api endpoint. For example /api/vault")
+          .setValue(this.plugin.settings.endpoint)
+          .onChange(async (value) => {
+            this.plugin.settings.endpoint = value;
+            await this.plugin.saveSettings();
+          })
+      );
+  }
+}
