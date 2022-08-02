5a8b39d998f294e1846f5f1b3ed0a700c7535d16
CobyPear <pear.coby@gmail.com>
2022-08-01 21:36:34 -0500

    Create new vault if none found, update notes if found vault

--- server/controllers/vault.ts
+++ server/controllers/vault.ts
@@ -1,5 +1,48 @@
+import type { Node, Vault } from "@prisma/client";
 import { prisma } from "../db";
 import type { Request, Response } from "express";
+
+const createOrUpdateNodes = async (
+  nodes: Node[],
+  vaultId: Vault["id"],
+  create = false
+) => {
+  for (const { content, name, extension, path } of nodes) {
+    const data = {
+      content: content,
+      name: name,
+      extension: extension,
+      path: path,
+    };
+    if (create) {
+      await prisma.node.create({
+        data: {
+          Vault: {
+            connect: { id: vaultId },
+          },
+          ...data,
+        },
+      });
+    } else {
+      await prisma.node.updateMany({
+        where: {
+          name: name,
+        },
+        data: {
+          ...data,
+        },
+      });
+    }
+  }
+  return await prisma.vault.findUnique({
+    where: {
+      id: vaultId,
+    },
+    include: {
+      nodes: true,
+    },
+  });
+};
 
 export const vaultControllers = {
   get: async (req: Request, res: Response) => {
@@ -18,28 +61,34 @@ export const vaultControllers = {
     const {
       body: { nodes, vault },
     } = req;
-    console.log("nodes, vault", nodes, vault);
+    let resultVault;
     if (nodes && vault) {
-      const newVault = await prisma.vault.create({
-        data: {
+      const [foundVault] = await prisma.vault.findMany({
+        where: {
           name: vault,
         },
+        include: {
+          nodes: true,
+        },
       });
-      for (const { content, name, extension, path } of nodes) {
-        await prisma.node.create({
+
+      if (foundVault) {
+        console.log("Found vault. Adding nodes...");
+        resultVault =
+          foundVault.nodes.length === 0
+            ? // create the nodes if there are none!
+              await createOrUpdateNodes(nodes, foundVault.id, true)
+            : // else, update them
+              await createOrUpdateNodes(nodes, foundVault.id);
+      } else {
+        const newVault = await prisma.vault.create({
           data: {
-            Vault: {
-              connect: { id: newVault.id },
-            },
-            content: content,
-            name: name,
-            extension: extension,
-            path: path,
+            name: vault,
           },
         });
+        resultVault = await createOrUpdateNodes(nodes, newVault.id, true);
       }
-      console.log("newVault", newVault);
-      res.json({ message: "success" });
+      res.json({ message: "success", vault: resultVault });
     } else {
       res.status(400).json({ error: "No vault was received" });
     }
