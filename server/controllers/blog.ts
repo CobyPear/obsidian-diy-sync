342103a69c7b6bde665e0f3e5a92902f62c08f57
CobyPear <shagsher@gmail.com>
2023-02-07 22:10:33 -0600

    Send #unpublished and #deleted posts to be handled by the frontend to send proper 400 code

--- server/controllers/blog.ts
+++ server/controllers/blog.ts
@@ -1,74 +1,76 @@
-import { prisma } from "../db";
-import type { Node } from "@prisma/client";
-import type { Request, Response } from "express";
+import { prisma } from '../db';
+import type { Node } from '@prisma/client';
+import type { Request, Response } from 'express';
 
 export const blogControllers = {
-  get: async (req: Request, res: Response) => {
-    const { vault } = req.query;
-    if (!vault) {
-      return res.status(400).json({
-        message:
-          "No vault name provided.\nThe vault name should be available as a query string parameter vault=vaultName ",
-      });
-    }
+	get: async (req: Request, res: Response) => {
+		const { vault } = req.query;
+		if (!vault) {
+			return res.status(400).json({
+				message:
+					'No vault name provided.\nThe vault name should be available as a query string parameter vault=vaultName ',
+			});
+		}
 
-    try {
-      const vaultFromDb = await prisma.vault.findFirst({
-        where: {
-          name: vault as string,
-        },
-        include: {
-          nodes: true,
-        },
-      });
+		try {
+			const vaultFromDb = await prisma.vault.findFirst({
+				where: {
+					name: vault as string,
+				},
+				include: {
+					nodes: true,
+				},
+			});
 
-      if (!vaultFromDb) {
-        return res.status(404).json({
-          message: `Vault named ${vault} was not found in the DB. Please make sure the vault exists in the database`,
-        });
-      }
+			if (!vaultFromDb) {
+				return res.status(404).json({
+					message: `Vault named ${vault} was not found in the DB. Please make sure the vault exists in the database`,
+				});
+			}
 
-      const publishedNodes = vaultFromDb?.nodes
-        .filter(({ content }: Node) => {
-          return (
-            content.includes("published: true") ||
-            content.includes("#published")
-          );
-        })
-        .map(({ name, content, ctime, mtime }: Node) => {
-          const title = name.replace(/\.md$/g, "");
-          const slug = title.replace(/\s/g, "-").toLowerCase();
-          const createdAt = new Date(Number(ctime)).toLocaleDateString("en-US");
-          const modifiedAt = new Date(Number(mtime)).toLocaleDateString(
-            "en-US"
-          );
+			const publishedNodes = vaultFromDb?.nodes
+				.filter(({ content }: Node) => {
+					return (
+						content.includes('published: true') ||
+						content.includes('#published') ||
+						content.includes('#unpublished') ||
+						content.includes('#deleted')
+					);
+				})
+				.map(({ name, content, ctime, mtime }: Node) => {
+					const title = name.replace(/\.md$/g, '');
+					const slug = title.replace(/\s/g, '-').toLowerCase();
+					const createdAt = new Date(Number(ctime)).toLocaleDateString('en-US');
+					const modifiedAt = new Date(Number(mtime)).toLocaleDateString(
+						'en-US'
+					);
 
-          content = content.replace("#published", "");
-          return {
-            title,
-            slug,
-            content,
-            createdAt,
-            modifiedAt,
-          };
-        });
+					content = content.replace('#published', '');
+					return {
+						title,
+						slug,
+						content,
+						createdAt,
+						modifiedAt,
+					};
+				});
 
-      if (!publishedNodes) {
-        return res.status(404).json({
-          message: `Vault ${vault} has not published nodes. Please publish a node and try again.`,
-        });
-      }
+			if (!publishedNodes) {
+				return res.status(404).json({
+					message: `Vault ${vault} has not published nodes. Please publish a node and try again.`,
+				});
+			}
 
-      res.status(200).json(publishedNodes);
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({
-        message:
-          error instanceof Error
-            ? error.message
-            : "Something went wrong on the server",
-        error: error,
-      });
-    }
-  },
+			res.status(200).json(publishedNodes);
+		} catch (error) {
+			console.error(error);
+			return res.status(500).json({
+				message:
+					error instanceof Error
+						? error.message
+						: 'Something went wrong on the server',
+				error: error,
+			});
+		}
+	},
 };
--- server/utils/createOrUpdateNodes.ts
+++ server/utils/createOrUpdateNodes.ts
@@ -1,55 +1,48 @@
-import type { Node, Vault } from "@prisma/client";
-import { prisma } from "../db";
+import type { Node, Vault } from '@prisma/client';
+import { prisma } from '../db';
 
 export const createOrUpdateNodes = async ({
-  nodes,
-  vaultId,
+	nodes,
+	vaultId,
 }: {
-  nodes: Node[];
-  vaultId: Vault["id"];
+	nodes: Node[];
+	vaultId: Vault['id'];
 }) => {
-  for (const {
-    content,
-    name,
-    extension,
-    path,
-    ctime,
-    mtime,
-  } of nodes) {
-    const data = {
-      content,
-      name,
-      extension,
-      path,
-      ctime,
-      mtime,
-    };
+	for (const { content, name, extension, path, ctime, mtime } of nodes) {
+		const data = {
+			content,
+			name,
+			extension,
+			path,
+			ctime,
+			mtime,
+		};
 
-    try {
-      await prisma.node.upsert({
-        where: {
-          path: path,
-        },
-        create: {
-          ...data,
-          vaultId,
-        },
-        update: {
-          ...data,
-          vaultId,
-        },
-      });
-    } catch (error) {
-      return;
-    }
-  }
+		try {
+			await prisma.node.upsert({
+				where: {
+					path: path,
+				},
+				create: {
+					...data,
+					vaultId,
+				},
+				update: {
+					...data,
+					vaultId,
+				},
+			});
+		} catch (error) {
+			return;
+		}
+	}
 
-  return await prisma.vault.findUnique({
-    where: {
-      id: vaultId,
-    },
-    include: {
-      nodes: true,
-    },
-  });
+	return await prisma.vault.findUnique({
+		where: {
+			id: vaultId,
+		},
+		include: {
+			nodes: true,
+		},
+	});
 };
